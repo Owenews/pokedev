6891afa0c20e34e560d4dfcafdc413ffb13969c3
Owen <owenouiluobe@gmail.com>
2024-12-11 20:17:49 +0100

    file done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import HomePage from './pokemons/page/HomePage'
-import PokemonsListType from './pokemons/page/PokemonsListType'
-import ListTypes from './pokemons/page/ListTypes'
-
+import HomePage from './pokemons/page/HomePage/HomePage'
+import ListPokemonsType from './pokemons/page/ListPokemonsType/ListPokemonsType'
+import ListTypes from './pokemons/page/ListType/ListTypes'
+import ListPokemons from './pokemons/page/ListPokemons/ListPokemons'
+import RandomPokemon from './pokemons/page/RandomPokemon/RandomPokemon'
+import SearchResultPokemon from './pokemons/page/SearchResultPokemon/SearchResultPokemon'
+import PokemonDetails from './pokemons/page/PokemonDetails/PokemonDetails'
+import PokemonsFight from './pokemons/page/PokemonFight/PokemonsFight'
+import PokemonsType from './pokemons/page/PokemonsType/PokemonsType'
 
 
 function App() {
@@ -13,8 +17,16 @@ function App() {
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/type-list" element={<ListTypes />} />
-      <Route path="/pokemons-type-list/" element={<PokemonsListType />} />
+      <Route path="/pokemons-list" element={<ListPokemons />} />
+      <Route path="/pokemons-list/:name" element={<ListPokemons />} />
+      <Route path="/list-type" element={<ListTypes />} />
+      <Route path="/list-type:name" element={<ListTypes />} />
+      <Route path="/pokemons-type-list/:type?" element={<ListPokemonsType />} />
+      <Route path="/random-pokemon/" element={<RandomPokemon />} />
+      <Route path="/search-results/" element={<SearchResultPokemon />} /> 
+      <Route path="/pokemons-details/:name" element={<PokemonDetails />} />
+      <Route path="/pokemons-type/:type" element={<PokemonsType />} />
+      <Route path="/pokemon-fight" element={<PokemonsFight />} />
       </Routes>
       </BrowserRouter>
     </>
