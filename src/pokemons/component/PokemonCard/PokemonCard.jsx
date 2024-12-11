5b4977dec0ea1d51345da1fa4cf4655849953d81
Owen <owenouiluobe@gmail.com>
2024-12-11 20:22:09 +0100

    creation component PokemonCard

---
+++ src/pokemons/component/PokemonCard/PokemonCard.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import "./PokemonCard.scss"; // Assurez-vous d'avoir un fichier de style pour ce composant
+
+// eslint-disable-next-line react/prop-types
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <div key={pokemon.id} className="pokemon-card">
+      <h2 className="pokemon-title">{pokemon.name}</h2>
+      <img src={pokemon.image} alt={pokemon.name} className="pokemon-image" />
+      <div className="card-buttons">
+        <Link to={`/pokemons-details/${pokemon.name}`}>
+          <button className="card-button">Voir les détails</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonCard;
